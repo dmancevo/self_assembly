8fe2f633406d00bda7c6506c29501d6178eecb67
Diego A. Mancevo <dmancevo@gmail.com>
2016-05-08 19:47:09 +0000

    began template for kilobot simulation

---
+++ kilobot.py
@@ -0,0 +1,31 @@
+
+class Kilobot:
+
+  def __init__(self, bitmap):
+    
+    self.pos = None
+    self.gradient_value = None
+    self.localized = False
+    self.bitmap = bitmap
+    
+  def scan(self):
+    pass
+  
+  def broadcast(self):
+    pass
+  
+  def compute_gradient_value(self):
+    pass
+  
+  def localize(self):
+    pass
+  
+  def neighbors_gradient(self):
+    pass
+  
+  def on_edge(self):
+    pass
+  
+  def move(self):
+    pass
+  
