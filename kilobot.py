b9b65173dd0e39094c4c5c2efef408aaeef110e4
Diego A. Mancevo <dmancevo@gmail.com>
2016-05-09 13:36:19 +0000

    some commit

--- kilobot.py
+++ kilobot.py
@@ -1,21 +1,37 @@
+import random
 
 class Kilobot:
 
-  def __init__(self, ID, bitmap, pos=None, grad_val=None):
+  def __init__(self, ID, bitmap, pos=None, grad_val=float('inf'), radius=1):
     
-    self.ID        = ID
-    self.pos       = pos
-    self.grad_val  = grad_val
-    self.localized = False
-    self.bitmap    = bitmap
+    self.ID         = ID
+    self.pos        = pos
+    self.grad_val   = grad_val
+    self.bitmap     = bitmap
+    self.radius     = radius
+    self.stationary = True
     
   
   def update_gradient_val(self):
-    pass
+    """
+    Update gradient value to minimum among neighbors plus one.
+    """
+    
+    grad_vals = [s[2] for s in world.scan(self.ID)]
+    self.grad_val = min(grad_vals)+1
   
   def localize(self):
     pass
   
   def move(self):
-    pass
+    """
+    Return desired movement direction as a tuple.
+    """
+    
+    self.update_gradient()
+    
+    if self.stationary or random.uniform(0,1)>0.1:
+      return (0,0)
+    
+    
   
