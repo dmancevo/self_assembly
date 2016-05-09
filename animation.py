7379df0062f66095bd19a67bacc1ef6bd352d155
Olga Mikheeva <olga@mikheeva.org>
2016-05-09 15:28:53 +0200

    added orientation for animation

--- animation.py
+++ animation.py
@@ -10,7 +10,7 @@ RADIUS = 0.7
 swarmSize = 500
 fieldSizeX = 100
 fieldSizeY = 100
-tick = 500 # miliseconds
+tick = 100 # miliseconds
 
 def makeInitialFormation(swarmSize, radius, firstDim, offsetX, offsetY):
     swarmPos = np.zeros((2, swarmSize))
@@ -43,6 +43,7 @@ startPos = makeInitialFormation(swarmSize, RADIUS*1.2, 30, 10, 5)
 currentX = startPos[0,:]
 currentY = startPos[1,:]
 startPos = list(zip(currentX,currentY))
+startPosFace = list(zip(currentX,currentY + 0.75*RADIUS))
 
 #shape to assemble
 shapeX = np.array([0, 20, 20, 10, 10, 0, 0])
@@ -60,22 +61,36 @@ plt.gca().set_aspect('equal', adjustable='box')
 
 shape = ax.plot(shapeOffsetX + scaleX * shapeX, shapeOffsetY + scaleY * shapeY)
 
+
 circles = ax.add_collection(EllipseCollection(widths=2*RADIUS, heights=2*RADIUS, angles=0,
                                     units='xy', facecolors='blue',
                                     offsets=startPos, transOffset=ax.transData))
+points = ax.add_collection(EllipseCollection(widths=0.5*RADIUS, heights=0.5*RADIUS, angles=0,
+                                    units='xy', facecolors='black',
+                                    offsets=startPosFace, transOffset=ax.transData))
+
 def init():
     circles.set_offsets([])
+    points.set_offsets([])
     return circles
 
 def update(frame):
-    circles.set_offsets(getNewPositions())
+    updatePositions()
+    circles.set_offsets(list(zip(frame[0], frame[1])))
+    points.set_offsets(list(zip(frame[0], frame[1] + 0.75*RADIUS)))
     return circles
 
-def getNewPositions():
+def updatePositions():
     for i in range(swarmSize):
         currentX[i] += np.random.randn(1) * 0.05
         currentY[i] += np.random.randn(1) * 0.05
-    return list(zip(currentX, currentY))
+    return currentX, currentY
 
-ani = animation.FuncAnimation(fig, update, init_func=init, interval=tick)
+
+def data_gen():
+    while True:
+        yield updatePositions()
+
+#anim = animation.FuncAnimation(fig, update, data_gen, init_func=init, interval=tick)
+#anim.save('basic_animation.mp4', fps=int(1000/tick), extra_args=['-vcodec', 'libx264'])
 plt.show()
