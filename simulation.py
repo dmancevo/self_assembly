3edb9b464dbe44ddd80c8ba8c3a25060a369586d
Olga Mikheeva <olga@mikheeva.org>
2016-05-10 07:57:31 +0200

    Simulation

---
+++ simulation.py
@@ -0,0 +1,10 @@
+from world import World
+
+robotRadius = 0.7
+sensorRadius = 5
+swarmSize = 500
+tick = 100 # miliseconds
+velocity = 1
+
+
+World.initialize(swarmSize, robotRadius, sensorRadius, velocity, tick)
