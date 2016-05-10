9b37065df0a9bc8ab882d02d71eb49f8c6a43c79
Olga Mikheeva <olga@mikheeva.org>
2016-05-10 08:31:38 +0200

    Main loop, fast distance calculation.

--- simulation.py
+++ simulation.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.collections import EllipseCollection
 from world import World
 
 robotRadius = 0.7
@@ -6,5 +10,57 @@ swarmSize = 500
 tick = 100 # miliseconds
 velocity = 1
 
+fieldSizeX = 100
+fieldSizeY = 100
+
+#shape to assemble
+shapeX = np.array([0, 20, 20, 10, 10, 0, 0])
+shapeY = np.array([0, 0, 10, 10, 20, 20, 0])
+shapeOffsetX = 40
+shapeOffsetY = 40
+scaleX = 1
+scaleY = 1
 
 World.initialize(swarmSize, robotRadius, sensorRadius, velocity, tick)
+
+startPos = list(zip(World.positions[0,:],World.positions[1,:]))
+startPosFace = list(zip(World.positions[0,:],World.positions[1,:] + 0.75*robotRadius))
+
+
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+ax.set_xlim(0, fieldSizeX), ax.set_xticks([])
+ax.set_ylim(0, fieldSizeY), ax.set_yticks([])
+plt.gca().set_aspect('equal', adjustable='box')
+
+shape = ax.plot(shapeOffsetX + scaleX * shapeX, shapeOffsetY + scaleY * shapeY)
+
+
+circles = ax.add_collection(EllipseCollection(widths=2*robotRadius, heights=2*robotRadius, angles=0,
+                                    units='xy', facecolors='blue',
+                                    offsets=startPos, transOffset=ax.transData))
+points = ax.add_collection(EllipseCollection(widths=0.5*robotRadius, heights=0.5*robotRadius, angles=0,
+                                    units='xy', facecolors='black',
+                                    offsets=startPosFace, transOffset=ax.transData))
+
+def init():
+    circles.set_offsets([])
+    points.set_offsets([])
+    return circles
+
+
+def update(frame):
+    circles.set_offsets(list(zip(frame[0], frame[1])))
+    points.set_offsets(list(zip(frame[0], frame[1] + 0.75*robotRadius)))
+    return circles
+
+
+def mainLoop():
+    while True:
+        World.updateWorld()
+        yield World.positions[0,:],World.positions[1,:]
+        
+
+anim = animation.FuncAnimation(fig, update, mainLoop, init_func=init, interval=tick)
+#anim.save('basic_animation.mp4', fps=int(1000/tick), extra_args=['-vcodec', 'libx264'])
+plt.show()
--- world.py
+++ world.py
@@ -1,18 +1,112 @@
 #Here comes the physics engine (by Olga)
+import numpy as np
+from math import pi
+from kilobot import Kilobot
 
 class World:
   
-  def __init__(self):
-    pass
-  
-  
-  def scan(self,kilobot_id):
-    """
-    This method should be called by the kilobots to receive information
-    about their neighbors.
-    
-    Returns list of tuples where each tuple has (d, (x,y), grad_val, stationary).
-    (x,y) may be None and stationary is either True or False.
-    """
-    
-    return []
+    @classmethod
+    def initialize(cls, swarmSize, robotRadius, sensorRadius, velocity, tick=100):
+        """
+        tick is miliseconds between updates
+        """
+        cls.swarmSize = swarmSize
+        cls.radius = robotRadius
+        cls.sensor = sensorRadius
+        cls.tick = tick
+        cls.avgVelocity = velocity
+
+        cls.positions = cls.makeInitialFormation(50, 10, 10)
+        cls.orientations = np.random.rand(2, swarmSize) * 2 * pi #angle with X axes
+        cls.robots = []
+        for i in xrange(swarmSize):
+            cls.robots.append(Kilobot(i, None, radius=robotRadius))
+
+        noise = np.random.randn(swarmSize) * velocity * 0.1 # sigma is 10% of velocity
+        cls.velocities = np.fmax(velocity + noise, np.zeros((swarmSize))) #to aviod negative velocities
+        
+        cls.distances = np.array((swarmSize, swarmSize))
+        cls.updateDistances()
+
+        cls.inSensorRadius = np.zeros((swarmSize, swarmSize))
+        cls.updateInSensorRadius()
+
+
+    @classmethod
+    def makeInitialFormation(cls, firstDim, offsetX, offsetY):
+        swarmPos = np.zeros((2, cls.swarmSize))
+        shiftX = cls.radius * 1.2
+        shiftY = (3**0.5) * cls.radius * 1.2
+        x = 0
+        y = 0
+        row = 1
+        for i in xrange(cls.swarmSize):
+            swarmPos[0, i] = x
+            swarmPos[1, i] = y
+            
+            if (i+1) % firstDim == 0:
+                row += 1
+                y += shiftY
+                x = shiftX if row%2==0 else 0
+            else:
+                x += 2 * shiftX
+
+        #move
+        swarmPos[0,:] += offsetX
+        swarmPos[1,:] += offsetY
+
+        return swarmPos
+
+
+    @classmethod
+    def updateDistances(cls):
+        z = np.array([[complex(cls.positions[0,i], cls.positions[1,i]) for i in range(cls.positions.shape[1])]])
+        cls.distances = abs(z.T - z)
+
+
+    @classmethod
+    def updateInSensorRadius(cls):
+        #TODO: replace, very slow
+        cls.inSensorRadius = np.zeros((cls.swarmSize, cls.swarmSize))
+        for i in range(cls.distances.shape[0]):
+            for j in range(i, cls.distances.shape[1]):
+                if cls.distances[i,j] <= cls.sensor:
+                    cls.inSensorRadius[i,j] = 1
+                    cls.inSensorRadius[j,i] = 1
+
+
+    @classmethod
+    def updateEstimatedPositions():
+        #
+        pass
+
+    @classmethod
+    def updateWorld(cls):
+        """
+        this method is called each iteration from the main simulation loop
+        """
+        #TODO: ask all robots to make a move
+
+        #TODO: update their positions
+        
+        #update distances
+        cls.updateDistances()
+
+        #update in sensor radius matrix
+        #cls.updateInSensorRadius()
+
+        return cls.positions
+
+    @classmethod
+    def scan(cls, kilobot_id):
+        """
+        This method should be called by the kilobots to receive information
+        about their neighbors.
+
+        Returns list of tuples where each tuple has (d, (x,y), grad_val, stationary).
+        (x,y) may be None and stationary is either True or False.
+
+        """
+
+        return []
+        
