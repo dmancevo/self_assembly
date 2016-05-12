7b4b50115ec6b44f803cacddfd899101cc0a6643
Diego A. Mancevo <dmancevo@gmail.com>
2016-05-12 17:09:59 +0000

    quick fix on bitmap

--- bitmap.py
+++ bitmap.py
@@ -7,21 +7,22 @@ class BitMap:
     
     self.im = Image.open("shapes/{im}".format(im=image)).convert('L')
     self.arr = np.array(self.im.getdata(), np.uint8).reshape(self.im.size)
-  
+    
   def in_shape(self, pos):
     """
-    Determine if the point at coords (x,y)
+    Determine if the point at position pos
     is inside the figure or not.
     
     Returns boolean (True or False)
     """
     
-    if self.arr[pos] == 255:
-      return False
-    else:
-      return True
+    pos = (int(np.round(pos[0])),int(np.round(pos[1])))
+    
+    return self.arr[pos] != 255
   
 if __name__ == '__main__':
   
   bitmap = BitMap("shape1.png")
   print bitmap.in_shape((345,203))
+  
+  print bitmap.arr.shape
